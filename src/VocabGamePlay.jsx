e7eafbb41e3c2efc2223b3002334a97513ef8f9c
Kelly Burgess <kellyburgess91@gmail.com>
2018-12-05 16:18:54 -0700

    create game play compenent and set initial state for first selection and second selection

---
+++ src/VocabGamePlay.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class VocabGamePlay extends Component{
+    constructor(){
+		super();
+    this.state={
+        firstSelection: false,
+        secondSelection: false,
+    }
+
+}
+
+render(){
+    
+}
+
+
+}
