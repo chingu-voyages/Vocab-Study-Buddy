1bbd85566c3bda1e1f3901dff55fc24e03b75892
Kelly Burgess <kellyburgess91@gmail.com>
2018-12-05 19:01:19 -0700

    fixed formatting and passed props to constructor/super

--- src/VocabGamePlay.jsx
+++ src/VocabGamePlay.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
-class VocabGamePlay extends Component{
-    constructor(){
-		super();
-    this.state={
-        firstSelection: false,
-        secondSelection: false,
+class VocabGamePlay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstSelection: false,
+            secondSelection: false,
+        }
+
     }
 
-}
+    render() {
 
-render(){
-    
-}
+    }
 
 
 }
