042cd7eba1c058c05fe12af29e5c4aa6451baeb0
Kelly Burgess <kellyburgess91@gmail.com>
2018-12-07 16:10:10 -0700

    set up functions for game play

--- src/VocabGamePlay.jsx
+++ src/VocabGamePlay.jsx
@@ -4,10 +4,33 @@ class VocabGamePlay extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            firstSelection: false,
-            secondSelection: false,
+            firstSelection: null,
+            secondSelection: null,
         }
     }
+    handleClick(i){
+        if (this.state.firstSelection===null){
+            //set firstSelection to word value of card
+        }
+        else if (this.state.secondSelection===null){
+            //set secondSelection to word value of card
+
+            //call check match function: checkMatch(this.state.firstSelection, this.state.secondSelection);
+        }
+    }
+    checkMatch(first, second){
+        //check if word value for secondSelection is equal to translation value for first selection
+    }
+
+    inCorrectMatch(){
+        //set first and secondSelection to null
+    }
+
+    correctMatch(){
+        //set inactive for the matched cards to false
+        //set first and secondSelection to null
+    }
+
     render() {
     }
 }
