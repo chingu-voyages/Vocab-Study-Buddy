73be4bf9e23a3a3abdb19d6e3d614d9360d13ecf
AnutaK <akvaisman@gmail.com>
2018-12-08 23:46:21 -0800

    Added a WordCard component which is statically displaying the words from cards.json file. Also added a blank Title folder.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import Wrapper from './components/Wrapper';
+import WordCard from './components/WordCard';
+import cards from './cards.json'
+
 
 class App extends Component {
+  state = {
+    message: "Click an image to begin!",
+    cards: cards,
+   }
+  
   render() {
     return (
       <div className="App">
+        <Wrapper>                
+                {
+                    this.state.cards.map(card => (
+                        <WordCard
+                            word={card.word}
+                            selectCard={this.selectCard} 
+                          />
+                    ))
+                }
+        </Wrapper>
+
       </div>
     );
   }
---
+++ src/components/WordCard/WordCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./WordCard.css";
+
+const WordCard = props => (
+    <div className="card">
+        <div className="card-container">
+            <h3>{props.word}</h3>
+        </div>
+    
+    
+    </div>  
+);
+
+export default WordCard;
+
+
+
+
