657f0e7f98aae3045cbe16d7cfa7bd8bb3fb9513
Eva <evaplaysviolin@gmail.com>
2018-12-10 14:10:49 -0800

    Create dataset using Map for word pairs

---
+++ src/VocabGameData.js
@@ -0,0 +1,39 @@
+const basePairs = [
+  ["hello", "bonjour"],
+  ["goodbye", "au revoir"],
+  ["please", "s'il vous plaît"],
+  ["thank you", "merci"],
+  ["thank you very much", "merci beaucoup"],
+  ["you're welcome", "de rien"],
+  ["excuse me", "excusez-moi"],
+  ["yes", "oui"],
+  ["no", "non"],
+  ["can you help me, please?", "pouvez-vous m’aider, s’il vous plaît?"]
+];
+
+let dataset = [];
+function createDataset(base) {
+  base.forEach(function(array) {
+    dataset.push([
+      array[0],
+      {
+        word: array[0],
+        translation: array[1],
+        isSelected: null,
+        isCorrect: null
+      }
+    ],
+    [
+      array[1],
+      {
+        word: array[1],
+        translation: array[0],
+        isSelected: null,
+        isCorrect: null
+      }
+    ]);
+  });
+}
+
+createDataset(basePairs);
+let datasetMap = new Map(dataset);
