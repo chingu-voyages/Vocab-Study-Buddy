c821d8f126b1739e3e2cc67a0a96c4c1ddff18f3
serpient <littlegangwolf@gmail.com>
2018-12-12 16:22:38 -0800

    starter test file

---
+++ src/VocabGamePlay.test.js
@@ -0,0 +1,3 @@
+// test that each method function within component functions
+// make sure to test whether updating gameDataset in function 
+// (through initial state assignment) and methods changes the global dataset.
