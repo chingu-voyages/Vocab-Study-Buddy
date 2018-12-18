25f0f89280bd41744b8cb6f83c1554b5e4107c32
AnutaK <akvaisman@gmail.com>
2018-12-18 15:48:56 -0800

    tried to display the data

--- src/WordCard.jsx
+++ src/WordCard.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 // import VocabGameData from './VocabGameData';
 
 
-const WordCard = (props) => (
+const WordCard = (props) => {
+    console.log(props)
     return(
-      <div>
-          
+
+      <div>      
 
 
       </div>  
     )   
-);
+};
 
 export default WordCard;
