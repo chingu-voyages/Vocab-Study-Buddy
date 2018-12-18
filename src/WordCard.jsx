2463b9b0fb44ff95d06a368adab1e760cbcf87dd
AnutaK <akvaisman@gmail.com>
2018-12-18 15:33:55 -0800

    New branch

---
+++ src/WordCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import VocabGameData from './VocabGameData';
+
+
+const WordCard = (props) => (
+    return(
+      <div>
+          
+
+
+      </div>  
+    )   
+);
+
+export default WordCard;
