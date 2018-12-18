373b40c7ef325c6b55e385360a6e51572f4c065c
serpient <littlegangwolf@gmail.com>
2018-12-17 17:16:41 -0800

    add starter test

--- src/VocabGamePlay.test.js
+++ src/VocabGamePlay.test.js
@@ -1,3 +1,15 @@
-// test that each method function within component functions
-// make sure to test whether updating gameDataset in function 
-// (through initial state assignment) and methods changes the global dataset.
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import VocabGamePlay from './VocabGamePlay';
+import { GameDataset } from './VocabGameData';
+
+configure({ adapter: new Adapter() });
+
+test('State Change', () => {
+  const wrapper = shallow(<VocabGamePlay />);
+  wrapper.state().firstSelection = GameDataset.get('hello');
+  wrapper.state().secondSelection = GameDataset.get('hola');
+
+})
+
