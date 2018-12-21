45827a0cfc9c4c2984872aecb4b627b21ea1dcd5
serpient <littlegangwolf@gmail.com>
2018-12-21 01:32:27 -0800

    starter test units for VocabGamePlay

--- src/VocabGamePlay.test.js
+++ src/VocabGamePlay.test.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount } from 'enzyme';
 import VocabGamePlay from './VocabGamePlay';
 import { GameDataset } from './VocabGameData';
 
-configure({ adapter: new Adapter() });
+describe('clicking cards saves to state', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<VocabGamePlay />);
+  })
+  afterEach(() => {
+    wrapper.unmount();
+  })
+  test('first card saves to state', () => {
+    let firstItem = GameDataset.get('hello');
+    wrapper.find('.card').at(0).simulate('click');
+    expect(wrapper.state().firstSelection).toEqual(firstItem)
+  })
+  
+  test('clicking card twice nulls selection', () => {;
+    wrapper.find('.card').at(0).simulate('click');
+    wrapper.find('.card').at(0).simulate('click');
+    expect(wrapper.state().firstSelection).toEqual(null)
+  })
+});
 
-test('State Change', () => {
-  const wrapper = shallow(<VocabGamePlay />);
-  wrapper.state().firstSelection = GameDataset.get('hello');
-  wrapper.state().secondSelection = GameDataset.get('hola');
-
+describe('card match functions', () => {
+  let wrapper;
+  let firstItem = GameDataset.get('hello');
+  let secondItem = GameDataset.get('bonjour');
+  beforeEach(() => {
+    wrapper = mount(<VocabGamePlay />);
+    wrapper.find('.card').at(0).simulate('click');
+    wrapper.find('.card').at(1).simulate('click');
+  })
+  afterEach(() => {
+    wrapper.unmount();
+  })
+  test('first and second card saves to state', () => {
+    expect(wrapper.state().firstSelection).toEqual(firstItem)
+    expect(wrapper.state().secondSelection).toEqual(secondItem)
+  })
+  test('first and second card saves to state', () => {
+    expect(wrapper.state().firstSelection).toEqual(firstItem)
+    expect(wrapper.state().secondSelection).toEqual(secondItem)
+  })
 })
-
