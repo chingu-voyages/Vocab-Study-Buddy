9d36e16ec38c307869750166c3ef531a5127caf8
Eva <evaplaysviolin@gmail.com>
2019-01-16 15:15:45 -0800

    Move header, instructions, footer out of VocabGamePlay and into App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./App.scss";
 import VocabGamePlay from "./VocabGamePlay";
 import { GameDataset } from "./VocabGameData.js";
 
@@ -40,7 +41,22 @@ class App extends Component {
     return (
       <div className="App">
         { this.renderVocabButtons() }
+        <div id="header">VOCABULARY STUDY BUDDY</div>
+        <div id="instructions">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed feugiat neque quis ultricies rutrum. Sed ornare eu sapien a luctus.
+            Nullam nulla enim, vulputate in arcu nec, volutpat lobortis enim.
+            Nam nec finibus lorem, vitae tincidunt metus.
+            Cras fermentum erat vel massa finibus malesuada.
+            Maecenas quis luctus est.
+            Curabitur cursus ante eget nisl rutrum, in vulputate lacus scelerisque.
+            Praesent et vulputate lectus. Aenean mi purus, tincidunt eu semper eget,
+            efficitur vitae justo. Etiam nec rhoncus elit, vitae lacinia velit.
+          </p>
+        </div>
         <VocabGamePlay dataset={currentDataset} />
+        <div id="footer">show us love</div>
       </div>
     );
   }
--- src/VocabGamePlay.jsx
+++ src/VocabGamePlay.jsx
@@ -118,25 +118,10 @@ class VocabGamePlay extends Component {
     let { shuffledDataSet } = this.state;
     return (
       <div className="vocabGamePlay--container">
-        <div id="header">VOCABULARY STUDY BUDDY</div>
-        <div id="instructions">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed feugiat neque quis ultricies rutrum. Sed ornare eu sapien a luctus.
-            Nullam nulla enim, vulputate in arcu nec, volutpat lobortis enim.
-            Nam nec finibus lorem, vitae tincidunt metus.
-            Cras fermentum erat vel massa finibus malesuada.
-            Maecenas quis luctus est.
-            Curabitur cursus ante eget nisl rutrum, in vulputate lacus scelerisque.
-            Praesent et vulputate lectus. Aenean mi purus, tincidunt eu semper eget,
-            efficitur vitae justo. Etiam nec rhoncus elit, vitae lacinia velit.
-          </p>
-        </div>
         <div id="card-grid-container">
           {shuffledDataSet && this.renderGameCards(shuffledDataSet)}
           <div id="reset" onClick={() => this.resetGame(shuffledDataSet)}>↺</div>
         </div>
-        <div id="footer">show us love</div>
       </div>
     )
   }
